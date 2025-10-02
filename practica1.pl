4b0f5bb90073682833ccc9cd5002755911185a58
Facundo Ava <Facundoa013@gmail.com>
2025-10-02 11:55:45 -0300

    Primer commit

---
+++ practica1.pl
@@ -0,0 +1,30 @@
+maximo(X,Y,M) :- (X >= Y -> M=X; M=Y).
+
+
+primero([Y|_],Y).
+    %primero([X, b, c], a).     -> X = a.
+    %primero([X, Y], a).        -> X = a.
+    %primero(X, a).             -> X = [a|_].
+
+cola([_|X],X).
+    %cola([a|L], [b, c]). -> L = [b, c].
+    %cola(L, [b, c]). -> L = [_, b, c].
+cons(E,A,[E|A]).
+    %cons(X, [b, c], [a, b, c]).   -> X = a.
+    %cons(a, L, [a, b, c]).        -> L = [b, c].
+    %cons(b, L, [a, b, c]).        -> FALSE
+    %cons(X, L, [a, b, c]).        ->X = a,
+    %                                L = [b, c].
+suma_lista([], 0).
+suma_lista([X|Y], Total) :-
+    suma_lista(Y, Subtotal),
+    Total is X + Subtotal.
+
+
+
+pertenece(X,[X|_]).
+pertenece(X,[_|Y]):- pertenece(X,Y).
+
+    % Generar las consultas
+    % pertenece(c,[a,c,b,c,d]).
+    % pertenece(X, [a,b,c]), pertenece(X, [d,c,b]).
