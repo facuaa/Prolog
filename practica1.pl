ddbc6cf72e9e791c5ba1c96f0658639378906417
Facundo Ava <Facundoa013@gmail.com>
2025-10-02 19:37:14 -0300

    a

--- practica1.pl
+++ practica1.pl
@@ -16,9 +16,8 @@ cons(E,A,[E|A]).
     %cons(X, L, [a, b, c]).        ->X = a,
     %                                L = [b, c].
 suma_lista([], 0).
-suma_lista([X|Y], Total) :-
-    suma_lista(Y, Subtotal),
-    Total is X + Subtotal.
+suma_lista([X|Y], R) :-
+    suma_lista(Y, R1), R is X + R1.
 
 
 
@@ -31,6 +30,28 @@ pertenece(X,[_|Y]):- pertenece(X,Y).
 
 
 conc([],L,L).
+conc([X|R1],L,[X|R]):-
+conc(R1,L,R).
 
-conc([X|R1],L2,[X|R]):-
- conc(R1,L2,R).
+    % I. ¿Qué lista hay que añadirle a la lista [a, b] para obtener [a, b, c, d]?
+        % [c,d]
+    % II. ¿Qué listas hay que concatenar para obtener [a, b]?
+        %[a],[b]
+    % III. ¿Pertenece b a la lista [a, b, c]?
+        % conc(_,[b|_],[a,b,c]).  <-----
+    % IV. ¿Es [b, c] una sublista de [a, b, c, d]?
+        %
+    % V. ¿Es [b, d] una sublista de [a, b, c, d]?
+        %
+    % VI. ¿Cuál es el último elemento de [b, a, c, d]?
+        % con esto se consigue la cola conc(_,[_|L],[a,b,c]). 
+        %
+
+% palindromo es lo mismo escribir de adelnta a atras que de atras para adelante. ej: oso --->> [o,s,o].
+
+palindromo([]).
+palindromo([_]).
+palindromo([X|Xs]) :-
+    cola(Xs, U, Medio), X = U, palindromo(Medio).
+%duplicar([],R).
+%duplicar([X|Y],[X|X]):- duplicar(Y,[X|X]).
